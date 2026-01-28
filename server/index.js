5d8ed8d9c569a1e550b66f32c4923fb6dd71b4fa
jonas <j.picot@semep.nc>
2026-01-29 10:27:17 +1100

    Ajout index.js pour le plugin Gree

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// index.js du plugin Gree
+const { connectGateway, readBits } = require("./server/driver_gree");
+const { scanUnits } = require("./server/autdetect");
+
+module.exports = {
+    name: "gree",
+    description: "Plugin Gree pour auto-détection unités Modbus TCP",
+    connectGateway,
+    readBits,
+    scanUnits
+};
