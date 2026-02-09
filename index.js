fd59060d25e2fc9a6d19d02d8d09ed6f318b929c
jojo687a <162922965+jojo687a@users.noreply.github.com>
2026-02-09 15:16:31 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-// index.js du plugin Gree
-const { connectGateway, readBits } = require("./server/driver_gree");
-const { scanUnits } = require("./server/autdetect");
-
 module.exports = {
-    name: "gree",
-    description: "Plugin Gree pour auto-détection unités Modbus TCP",
-    connectGateway,
-    readBits,
-    scanUnits
+    name: "Gree Modbus Plugin",
+    version: "1.0.0"
 };
 
-=======
-const apiGree = require('./server/api_gree');
-module.exports = {
-    name: "gree",
-    description: "Plugin Gree Web",
-    api: apiGree
-};
->>>>>>> e494626 (Update plugin)
